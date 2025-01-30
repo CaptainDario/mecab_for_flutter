8ec01c37b5f52baf68b5d66abf5460ccd31e9d81
dario windows arm64 <>
2025-01-30 12:29:40 -0800

    load windows libraries from the plugin not application

--- lib/lib_mecab.dart
+++ lib/lib_mecab.dart
@@ -1,13 +1,14 @@
 // Dart imports:
 import 'dart:async';
+import 'dart:ffi';
 
 // Flutter imports:
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 
 // Package imports:
 import 'package:universal_ffi/ffi_helper.dart';
 import 'package:universal_io/io.dart';
-import 'package:path/path.dart' as p;
 
 
 
@@ -23,10 +24,19 @@ Future<FfiHelper> loadMecabDartLib () async {
     ffiHelper = (await FfiHelper.load("mecab_dart.so", options: {LoadOption.isFfiPlugin}));
   }
   else if(Platform.isWindows) {
+    String architecture = () {
+      if(Abi.current() == Abi.windowsArm64){
+        return "arm64";
+      }
+      else if(Abi.current() == Abi.windowsX64){
+        return "x86";
+      }
+      else{
+        throw PlatformException(code: "This platform is not supported by mecab_for_flutter!");
+      }
+    } ();
     ffiHelper = (await FfiHelper.load(
-      p.joinAll([
-        Directory(Platform.resolvedExecutable).parent.path, "blobs", "libmecab.dll"
-      ]),
+      "libmecab_${architecture}.dll"
     ));
   }
   else {
