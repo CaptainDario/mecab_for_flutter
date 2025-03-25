fec556553f0d0377bd8e04c33859e3ff0c3c3f78
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-03-25 11:58:14 +0100

    removed web related codr from native

--- lib/lib_mecab_native.dart
+++ lib/lib_mecab_native.dart
@@ -2,10 +2,6 @@
 import 'dart:async';
 import 'dart:ffi';
 
-// Flutter imports:
-import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
-
 // Package imports:
 import 'package:universal_ffi/ffi_helper.dart';
 import 'package:universal_io/io.dart';
@@ -17,10 +13,7 @@ Future<FfiHelper> loadMecabDartLib () async {
 
   FfiHelper ffiHelper;
 
-  if (kIsWeb) {
-    ffiHelper = (await FfiHelper.load("assets/blobs/libmecab.js"));
-  }
-  else if(Platform.isAndroid){
+  if(Platform.isAndroid){
     // 'lib' is automatically added to the library name
     ffiHelper = (await FfiHelper.load("mecab_dart.so", options: {LoadOption.isFfiPlugin}));
   }
@@ -33,7 +26,7 @@ Future<FfiHelper> loadMecabDartLib () async {
         return "x86";
       }
       else{
-        throw PlatformException(code: "This platform is not supported by mecab_for_flutter!");
+        throw Exception("This platform is not supported by mecab_for_flutter!");
       }
     } ();
     ffiHelper = (await FfiHelper.load(
